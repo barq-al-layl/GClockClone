239f3e803ebdecaa2ad27b800a1d7c0dcd76312a
barq-al-layl <moh74s2001@gmail.com>
2023-01-03 21:24:14 +0200

    resizeable text

--- app/src/main/java/com/ba/gclockclone/feature/timer/ui/ActiveTimerCard.kt
+++ app/src/main/java/com/ba/gclockclone/feature/timer/ui/ActiveTimerCard.kt
@@ -10,12 +10,11 @@ import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
@@ -105,12 +104,27 @@ fun ActiveTimerCard(
 								),
 							)
 						}
-					},
+					}
+					.padding(horizontal = 16.dp),
 				contentAlignment = Alignment.Center,
 			) {
+				var shouldDrawContent by remember { mutableStateOf(false) }
+				var fontSize by remember { mutableStateOf(72.sp) }
 				Text(
 					text = time,
-					fontSize = 48.sp,
+					fontSize = fontSize,
+					softWrap = false,
+					modifier = Modifier.drawWithContent {
+						if (shouldDrawContent)
+							drawContent()
+					},
+					onTextLayout = {
+						if (it.didOverflowWidth) {
+							fontSize *= 0.95
+						} else {
+							shouldDrawContent = true
+						}
+					}
 				)
 			}
 			Row(
